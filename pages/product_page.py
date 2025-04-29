9cdfc15bc04e8c46b3e81606487eb4409e042668
Fogelion <finoleggen@gmail.com>
2025-04-29 20:26:36 +0300

    проверка названий книг на нескольких страницах подряд

--- pages/product_page.py
+++ pages/product_page.py
@@ -5,10 +5,13 @@ import time
 
 
 class ProductPage(BasePage):
+    total_books_price = 0
+
     def add_product_to_cart(self):
         cart_add = self.browser.find_element(*ProductPageLocators.CART_ADD)
         cart_add.click()
         self.solve_quiz_and_get_code()
+        # time.sleep(1)
 
     def should_be_cart_button(self):
         assert self.is_element_present(*ProductPageLocators.CART_ADD), "Cart button is not presented"
@@ -21,9 +24,13 @@ class ProductPage(BasePage):
         assert book_name.text == book_name_add.text, "Book name not equal to cart book name"
 
     def should_be_same_book_price(self):
-        book_price = self.browser.find_element(*ProductPageLocators.BOOK_PRICE).text
-        print(f"Цена книги: {book_price}")
-        cart_price_elem = self.browser.find_element(*ProductPageLocators.CART_PRICE).text
-        cart_price = re.search(r'£[\d,]+\.\d{2}', cart_price_elem).group()
-        print(f"Стоимость корзины: {cart_price}")
-        assert book_price == cart_price, "Book price not equal to cart price"
+        book_price_elem = self.browser.find_element(*ProductPageLocators.BOOK_PRICE)
+        book_price = round(float(book_price_elem.text[1:]), 2)
+        print(f"Цена книги: £{book_price}")
+        ProductPage.total_books_price += book_price
+        print(f"Текущая сумма всех книг: £{ProductPage.total_books_price}")
+        cart_price_elem = self.browser.find_element(*ProductPageLocators.CART_PRICE)
+        cart_price = float(re.search(r'[\d,]+\.\d{2}', cart_price_elem.text).group())
+        print(f"Стоимость корзины: £{cart_price}")
+        assert abs(cart_price - ProductPage.total_books_price) < 0.01, \
+                f"Cart price {cart_price} not equal to total books price {self.total_books_price}"
