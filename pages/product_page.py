b795897fef31be95b8dea1d9925b42c83c2d51c4
Fogelion <finoleggen@gmail.com>
2025-04-30 11:10:31 +0300

    проверки на наличие, появление и исчезновение сообщения о добавление в корзину

--- pages/product_page.py
+++ pages/product_page.py
@@ -43,3 +43,7 @@ class ProductPage(BasePage):
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.BOOK_NAME_ADD), \
             "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.BOOK_NAME_ADD), \
+            "Success message is presented, but should be disappeared"
--- test_product_page.py
+++ test_product_page.py
@@ -7,20 +7,28 @@ from .pages.product_page import ProductPage
 import pytest
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_cart(browser, link):
-    link = link
-# def test_guest_can_add_product_to_cart(browser):
-#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2"
+# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+#                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+# def test_guest_can_add_products_to_cart(browser, link):
+#     link = link
+#     product_page = ProductPage(browser, link)
+#     product_page.open()
+#     product_page.should_not_be_success_message()
+#     product_page.should_be_cart_button()
+#     product_page.add_product_to_cart()
+#     product_page.should_be_same_book_name()
+#     product_page.should_be_same_book_price()
+
+def test_guest_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_not_be_success_message()
@@ -28,3 +36,25 @@ def test_guest_can_add_product_to_cart(browser, link):
     product_page.add_product_to_cart()
     product_page.should_be_same_book_name()
     product_page.should_be_same_book_price()
+
+def test_guest_cant_see_success_message_after_adding_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_cart_button()
+    product_page.add_product_to_cart()
+    product_page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+def test_message_disappeared_after_adding_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_be_cart_button()
+    product_page.add_product_to_cart()
+    product_page.should_be_disappeared_success_message()
